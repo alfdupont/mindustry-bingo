5dd4f3b65b7ba406d71f371b59db26bc847f78c3
Aurélien GIRAUD <giraudan@skysoft-atm.com>
2024-04-16 15:06:42 +0200

    Revert "moved everything to 'public'"
    This reverts commit d11f477bacca8c67d2682278fd04e8bde5b749c2.

--- scripts/make_sprites_db.sh
+++ scripts/make_sprites_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../public/assets/sprites" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../bingo/assets/sprites" >/dev/null 2>&1 && pwd )"
 cd "${PROJECT_DIR}" || exit 1
 for i in *; do echo "    "'"'"${i%\.png}"'"'": "'"'"assets/sprites/${i}"'"'","; done
